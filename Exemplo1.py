6aea59a6f96ecf9ba2af5fadea9682fb34a886d3
Aluno <dionathanfrancisco17@gmail.com>
2026-06-16 09:05:01 -0300

    Exemplo 1

---
+++ Exemplo1.py
@@ -0,0 +1,42 @@
+#Descobrindo os tipos de dados
+
+idade= 18
+
+altura= 1.75
+
+nome= "João"
+
+aprovado= True
+
+frutas= ("Maça", "Banana", "Uva")
+
+cores= ("Azul", "Verde", "Vermelho")
+
+aluno= {"nome": "Maria",
+        "Idade": 18,
+        "Curso": "Desenvolvimento de sistemas"
+        }
+
+
+#print(Exemplos de tipos de dados e python\n)
+
+print("Idade:", idade)
+print("Tipo:", type(idade))
+
+print("\nAltura:", altura)
+print("Tipo:", type(altura))
+
+print("\nnome:", nome)
+print("tipo:", type(idade))
+
+print("\naprovado:", aprovado)
+print("Tipo:", type(aprovado))
+
+print("\nfrutas:", frutas)
+print("Tipo:", type(frutas))
+
+print("\ncores:", cores)
+print("Tipo:", type(cores))
+
+print("\naluno:", aluno)
+print("Tipo:", type(aluno))
