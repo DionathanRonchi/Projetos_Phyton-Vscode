8027d66f00f47fc16a35b79e2b75ddbe64b46e62
DionathanRonchi <dionathanfrancisco17@gmail.com>
2026-08-27 09:22:46 -0300

    Atua

--- MinhaLoja_flask_Exercicio/app_minha.py
+++ MinhaLoja_flask_Exercicio/app_minha.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 # CRIAR A APLICAÇAO FLASK
 app = Flask(__name__)
@@ -6,21 +6,28 @@ app = Flask(__name__)
 # DEFINE UMA ROTA
 @app.route('/')
 def index():
-    return "Olá! Minha loja OK."
+    return render_template('index_minha.html')
 
 @app.route('/sobre')
 def sobre():
-    return "Esta é a página sobre a minha loja!!."
+    return render_template('sobre_minha.html')
 
 
 @app.route("/produto/<int:id>")
 def produto(id):
-    return f"Exibindo produto com ID {id}."
+    return render_template('produto_minha.html', id=id)
+
 
 @app.route("/produtos")
 def produtos():
-    return f"1. Notebook - R$ 3.500,00\n2. Smartphone - R$ 1.800,00\n3. Fone de ouvido - R$ 150,00"
-
+   lista = [
+       {"id": 1, "nome": "Teclado", "preco": 500.00, "categoria": "Eletronicos"},
+       {"id": 2, "nome": "Colher", "preco": 30.50, "categoria": "Cozinha"},
+       {"id": 3, "nome": "Condicionador", "preco": 7.99, "categoria": "Banheiro"},
+       {"id": 4, "nome": "Camiseta", "preco": 50.00, "categoria": "Vestuário"},
+       {"id": 5, "nome": "Cadeira", "preco": 150.00, "categoria": "Móveis"}
+       ]
+   return render_template('produtos_minha.html', produtos=lista)
 
 #INICIAR SERVIDOR FLASK
 if __name__ == '__main__':
